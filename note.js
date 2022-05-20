dcaa6f632c7b44e51659250c306af63fbd4b04f1
Manaheeljamil <manaheeljamil20@gmail.com>
2022-05-20 03:53:36 -0700

    First commit

---
+++ note.js
@@ -0,0 +1,56 @@
+// CHAPTER # 26 - 30:
+// Question # 1:
+// var number = "3.45214";
+// var round = Math.round(3.45214);
+// var floor = Math.floor(3.45214);
+// var ceil = Math.ceil(3.45214);
+// document.write("Number:" + " "+ number+"<br/>" +"Round off value" + " " + round+"<br/>"+"Floor value:" + " " + floor + "<br/>" + "Ceil value" + " " + ceil)
+
+// Question # 2:
+// var number = "-2.673";
+// var round = Math.round(-2.673);
+// var floor = Math.floor(-2.673);
+// var ceil = Math.ceil(-2.673);
+// document.write("Number:" + " "+ number+"<br/>" +"Round off value" + " " + round+"<br/>"+"Floor value:" + " " + floor + "<br/>" + "Ceil value" + " " + ceil)
+
+// Question # 3:
+// var absolute = "-4";
+// var value = Math.abs(-4);
+// document.write("The absolute value of "+ " "+ absolute+ " " + "is" + " "+value)
+
+// Question # 4; 
+// var dice = Math.floor( Math.random() * 6 ) +1;
+// document.write("Random dice value: 4"+"<br/>");
+// document.write("Random dice value:"+ " "+ dice);
+
+// Question # 5:
+// var coin = Math.floor( Math.random() * 2 ) +1;
+// document.write("2 <br> random coin value:Heads"+"<br/>");
+// document.write(coin+"<br> random coin value:Tails");
+
+// Question #6:
+// var random = Math.floor(Math.random() * 10) + 1 
+// document.write("Random number between 1 to 100 :" + " " + random);
+
+// Question # 7:
+// var weight = prompt("Enter your weight in kilograms");
+// var array =["50", "50kgs", "50.2kgs", "50.2"];
+// for (var i =0; i<=array.length; i++) {
+//     if (weight === array[i]) {
+//         document.write("The weight of user is "+" "+ weight+ " "+ "Kilogram");
+//         break;
+//     }
+// }
+// if  (weight !== array[i]) {
+//     alert("Not Allowed!");
+// }
+
+// Question # 8:
+// var number = prompt("Enter a number between 1 to 10?")
+// var secNum = "4";
+//     if (secNum === number) {
+//         alert("Congratulations!!");
+//     }
+// if (secNum !== number) {
+//     alert("Try again!");
+// }
